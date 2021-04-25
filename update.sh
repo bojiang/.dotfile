813f6eff1b7ab4ecdf6db70812984ad738a7ab83
bojiang <hrmthw@outlook.com>
2021-04-25 11:44:44 +0800

    dotupdate: update coc plugins

--- update.sh
+++ update.sh
@@ -7,4 +7,4 @@ git submodule update
 #cd $HOME/.dotfile/.config/coc/extensions
 #npm install
 cd $_PWD
-vim +CocUpdateSync +qall  # update coc plugins
+vim -c 'CocUpdateSync|q'  # update coc plugins
