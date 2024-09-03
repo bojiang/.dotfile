10bb73bd390a385eabb59818d76bd344a11b1e91
bojiang <bojiang_@outlook.com>
2024-09-03 16:13:08 +0800

    install lua; refactor nvim init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,26 +5,96 @@ vim.g.maplocalleader = "\\"
 
 require("lazy").setup({
   spec = {
-    {"neovim/nvim-lspconfig"},
+    { -- ls & format
+      "neovim/nvim-lspconfig",
+      dependencies = {
+        "lukas-reineke/lsp-format.nvim",
+        "nmac427/guess-indent.nvim",
+      },
+      config = function()
+        require('guess-indent').setup {}
+        require("lsp-format").setup {}
+        require("lspconfig").pyright.setup { on_attach = require("lsp-format").on_attach }
+        require("lspconfig").lua_ls.setup { on_attach = require("lsp-format").on_attach }
+      end
+    },
+    { -- tree
+      "nvim-tree/nvim-tree.lua",
+      config = function()
+        require("nvim-tree").setup({
+          view = {
+            width = 40,
+            side = 'left',
+          },
+          git = {
+            enable = true,
+          },
+          renderer = {
+            icons = {
+              show = {
+                git = true,
+                folder = true,
+                file = true,
+                folder_arrow = true,
+              },
+            },
+          },
+          on_attach = function(bufnr)
+            local api = require('nvim-tree.api')
+            local opts = { buffer = bufnr, noremap = true, silent = true, nowait = true }
 
-    { "nvim-tree/nvim-tree.lua" },
-    {
-      'nvim-telescope/telescope.nvim', tag = '0.1.8',
-      dependencies = { 
+            vim.keymap.set('n', '<C-e>', api.tree.toggle, opts)
+            vim.keymap.set('n', 'l', api.node.open.edit, opts)
+            vim.keymap.set('n', '<2-LeftMouse>', api.node.open.edit, opts) -- 双击左键打开文件或目录
+            -- vim.keymap.set('n', 'v', api.node.open.vertical, opts)         -- v 键垂直分割窗口打开文件
+            -- vim.keymap.set('n', 's', api.node.open.horizontal, opts)       -- s 键水平分割窗口打开文件
+            vim.keymap.set('n', 'I', api.tree.toggle_hidden_filter, opts) -- I 键切换隐藏文件显示
+            vim.keymap.set('n', 'R', api.tree.reload, opts)               -- R 键重新加载文件树
+            vim.keymap.set('n', '<Space>', api.node.open.preview, opts)
+          end
+        })
+      end
+    },
+    { -- lists
+      'nvim-telescope/telescope.nvim',
+      tag = '0.1.8',
+      dependencies = {
         'nvim-lua/plenary.nvim',
         'nvim-telescope/telescope-frecency.nvim',
-      }
+      },
+      config = function()
+        local telescope = require('telescope')
+        telescope.setup({
+          extensions = {
+            frecency = {
+              default_workspace = 'CWD',
+              show_unindexed = true,
+              ignore_patterns = { "*.git/*", "*/tmp/*", ".venv/*" },
+              default_text = '',
+            }
+          },
+          defaults = {
+            mappings = {
+              i = {
+                ["<C-j>"] = "move_selection_next",
+                ["<C-k>"] = "move_selection_previous",
+              },
+            },
+          },
+        })
+        telescope.load_extension('frecency')
+      end
     },
     {
       "nvim-treesitter/nvim-treesitter",
       build = ":TSUpdate",
       config = function()
         require('nvim-treesitter.configs').setup({
-            ensure_installed = { "c", "lua", "python" },
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
+          ensure_installed = { "c", "lua", "python" },
+          highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+          },
         })
       end
     },
@@ -36,19 +106,28 @@ require("lazy").setup({
         -- add any opts here
       },
       build = ":AvanteBuild", -- This is optional, recommended tho. Also note that this will block the startup for a bit since we are compiling bindings in Rust.
+      config = function()
+        require("copilot").setup({
+          suggestion = {
+            enabled = true,
+            auto_trigger = true,
+          },
+          filetypes = {
+            ["*"] = true,
+          }
+        })
+      end,
       dependencies = {
         "stevearc/dressing.nvim",
         "nvim-lua/plenary.nvim",
         "MunifTanjim/nui.nvim",
-        --- The below dependencies are optional,
         "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-        "zbirenbaum/copilot.lua", -- for providers='copilot'
+        "zbirenbaum/copilot.lua",      -- for providers='copilot'
         {
           -- support for image pasting
           "HakonHarnes/img-clip.nvim",
           event = "VeryLazy",
           opts = {
-            -- recommended settings
             default = {
               embed_image_as_base64 = false,
               prompt_for_file_name = false,
@@ -61,7 +140,6 @@ require("lazy").setup({
           },
         },
         {
-          -- Make sure to setup it properly if you have lazy=true
           'MeanderingProgrammer/render-markdown.nvim',
           opts = {
             file_types = { "markdown", "Avante" },
@@ -76,83 +154,19 @@ require("lazy").setup({
   checker = { enabled = true },
 })
 
--- lsp
-require'lspconfig'.pyright.setup{}
 
-
--- nvim.tree
-require("nvim-tree").setup({
-  view = {
-    width = 30,
-    side = 'left',
-  },
-  git = {
-    enable = true,
-  },
-  renderer = {
-    icons = {
-      show = {
-        git = true,
-        folder = true,
-        file = true,
-        folder_arrow = true,
-      },
-    },
-  },
-  on_attach = function(bufnr)
-    local api = require('nvim-tree.api')
-    local opts = { buffer = bufnr, noremap = true, silent = true, nowait = true }
-
-    vim.keymap.set('n', '<C-e>', api.tree.toggle, opts)
-    vim.keymap.set('n', '<CR>', api.node.open.edit, opts)           -- Enter 键打开文件或目录
-    vim.keymap.set('n', 'o', api.node.open.edit, opts)              -- o 键也可打开文件或目录
-    vim.keymap.set('n', '<2-LeftMouse>', api.node.open.edit, opts)  -- 双击左键打开文件或目录
-    vim.keymap.set('n', 'v', api.node.open.vertical, opts)          -- v 键垂直分割窗口打开文件
-    vim.keymap.set('n', 's', api.node.open.horizontal, opts)        -- s 键水平分割窗口打开文件
-    vim.keymap.set('n', 'I', api.tree.toggle_hidden_filter, opts)   -- I 键切换隐藏文件显示
-    vim.keymap.set('n', 'R', api.tree.reload, opts)                 -- R 键重新加载文件树
-
-    -- 其他自定义绑定
-    -- 比如绑定空格键来切换文件目录的折叠状态
-    vim.keymap.set('n', '<Space>', api.node.open.preview, opts)
-  end
-})
 
 
 -- VSCode theme
-vim.cmd[[colorscheme vscode]]
-vim.cmd[[let g:vscode_transparent = 1]]
-vim.cmd[[let g:vscode_italic_comment = 1]]
-
-
--- telescope
-local telescope = require('telescope')
-local telescope_builtin = require('telescope.builtin')
-
-telescope.setup({
-  extensions = {
-    frecency = {
-      default_workspace = 'CWD',
-      show_unindexed = true,
-      ignore_patterns = {"*.git/*", "*/tmp/*", ".venv/*"},
-      default_text = '',
-    }
-  },
-  defaults = {
-    mappings = {
-      i = {
-        ["<C-j>"] = "move_selection_next",
-        ["<C-k>"] = "move_selection_previous",
-      },
-    },
-  },
-})
-telescope.load_extension('frecency')
+vim.cmd [[colorscheme vscode]]
+vim.cmd [[let g:vscode_transparent = 1]]
+vim.cmd [[let g:vscode_italic_comment = 1]]
 
 
 
 -- key binds
-vim.keymap.set('n', '<C-f>', '<cmd>lua require("telescope").extensions.frecency.frecency()<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-f>', '<cmd>lua require("telescope").extensions.frecency.frecency()<CR>',
+  { noremap = true, silent = true })
 
 vim.keymap.set('n', '<C-g>', '', {
   noremap = true,
@@ -168,7 +182,7 @@ vim.keymap.set('n', '<C-g>', '', {
 })
 
 
-vim.keymap.set({'n', 'i'}, '<C-e>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.keymap.set({ 'n', 'i' }, '<C-e>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 
 vim.keymap.set('n', '<leader><Space>', ':AvanteAsk<CR>', { silent = true })
 
@@ -181,4 +195,3 @@ vim.keymap.set('i', '<C-h>', '<left>', { silent = true })
 vim.keymap.set('i', '<C-l>', '<right>', { silent = true })
 vim.keymap.set('i', '<C-j>', '<down>', { silent = true })
 vim.keymap.set('i', '<C-k>', '<up>', { silent = true })
-
--- install-req-w-apt.sh
+++ install-req-w-apt.sh
@@ -18,3 +18,32 @@ sudo ln -sf $INSTALL_DIR/bin/nvim /usr/local/bin/nvim
 sudo ln -sf $INSTALL_DIR/bin/nvim /usr/local/bin/vim
 rm nvim-linux64.tar.gz
 echo "nvim installed to /usr/local/bin/nvim"
+
+# python?
+read -p "是否安装Python语言支持？(y/n) " install_python
+
+if [ "$install_python" = "y" ]; then
+    echo "正在安装Python语言支持..."
+    sudo apt install npm -y
+    sudo npm install -g pyright
+    echo "Python语言支持已安装"
+else
+    echo "跳过Python语言支持安装"
+fi
+
+# lua?
+read -p "是否安装Lua语言支持？(y/n) " install_lua
+
+if [ "$install_lua" = "y" ]; then
+    echo "正在安装Lua语言支持..."
+    LUA_LS_URL="https://github.com/LuaLS/lua-language-server/releases/download/3.10.5/lua-language-server-3.10.5-linux-x64.tar.gz"
+    LUA_LS_DIR="/opt/lua-language-server"
+    curl -LO $LUA_LS_URL
+    sudo mkdir -p $LUA_LS_DIR
+    sudo tar xzf lua-language-server-3.10.5-linux-x64.tar.gz -C $LUA_LS_DIR
+    sudo ln -sf $LUA_LS_DIR/bin/lua-language-server /usr/local/bin/lua-language-server
+    rm lua-language-server-3.10.5-linux-x64.tar.gz
+    echo "Lua语言支持已安装"
+else
+    echo "跳过Lua语言支持安装"
+fi
